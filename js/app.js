2c3b8229f34a9a95601cba98f58283d1d9ff9cff
Vaugarni ryan <ryan.vaugarni@gmail.com>
2021-12-24 01:20:55 +0100

    ok

--- js/app.js
+++ js/app.js
@@ -1,51 +1,36 @@
 document.addEventListener("DOMContentLoaded",()=>{
-    
-    let s_btn, s_form, s_warning, s_btn_form, s_login, s_mdp
-    s_btn = document.querySelector(".connect")
-    s_form = document.querySelector(".form")
-    s_warning = document.querySelector(".warning")
-    s_btn_form = document.getElementsByTagName("input")[2]
-    s_login = localStorage.getItem("email")
-    s_mdp = localStorage.getItem("mdp")
-
-    s_btn.addEventListener("click",()=>{
-        console.log("Vous êtes logué")
-        s_form.classList.add('appear')
+    console.log("ok")
+    let form, warning, btn_form, traitement
+    form = document.querySelector(".form")
+    warning = document.querySelector(".warning")
+    btn_form = document.getElementsByTagName("input")[2]
+    console.log(btn_form)
+    /* affectation de valeur et de type */
+    console.log(btn.innerText)
+ 
+    btn.addEventListener("click",(e)=>{
+         form.classList.add('appear')
     })
-    
 
-    let connect=(e)=>{
+    traitement = (e)=>{
         e.preventDefault()
         let User = {
-     
-            login:document.getElementsByTagName("input")[0].value,
+            email:document.getElementsByTagName("input")[0].value,
             mdp:document.getElementsByTagName("input")[1].value,
         }
-        
-        if(s_login == User.login && s_mdp == User.mdp){
-            s_warning.style.display="block";
-            s_warning.classList.add("success")
-        //warning.classList.remove("success")
-        s_warning.textContent ="Vous êtes logué"
-        /* lien avec la 3ème page */
-        document.location.assign("accueil.html")
-            
+        if(!User.email || !User.mdp || !isNaN(User.email)){
+            warning.classList.add("warning")
+            warning.textContent ="Champs vides: remplir les champs!"
         }
-       
-        
         else{
-            s_warning.style.display="block";
-            s_warning.classList.add("warning")
-            s_warning.classList.remove("success")
-            s_warning.textContent ="Remplir les champs"
-            
+            sessionStorage.setItem("email", User.email);
+            localStorage.setItem("email", User.email)
+            localStorage.setItem("mdp", User.mdp)
+            /* lien avec la 2ème page */
+            document.location.assign("accueil.html")
         }
     }
-    /*s_btn_form.addEventListener("click",(e)=>{
-        connect()
-        window.location.assign("espace_membre.html")
-    })*/
-    s_btn_form.addEventListener("click", connect)
-    
+
+    btn_form.addEventListener("click", traitement)
 
 })
